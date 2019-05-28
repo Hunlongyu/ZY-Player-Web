a722d36b0cd3e1ade6f93c624864cb4714c18b6f
Hunlongyu <hunlongyu@gmail.com>
2019-05-28 22:18:44 +0800

    outputDir to doc

---
+++ doc/precache-manifest.f22d594647dfe7cedb3467047d034a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "1b14caa73d67116d2fe4",
+    "url": "js/chunk-vendors.04e8b37b.js"
+  },
+  {
+    "revision": "73713af600f873341ebe",
+    "url": "js/app.6e8c4729.js"
+  },
+  {
+    "revision": "2aaf1bcb94128140af1790c0cb2abfee",
+    "url": "index.html"
+  },
+  {
+    "revision": "1b14caa73d67116d2fe4",
+    "url": "css/chunk-vendors.e46cb91c.css"
+  },
+  {
+    "revision": "73713af600f873341ebe",
+    "url": "css/app.e8851917.css"
+  }
+];

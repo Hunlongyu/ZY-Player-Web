98ea020802393137947744b224a7f6ed19da2705
hunlongyu <hunlongyu@gmail.com>
2021-06-02 19:08:44 +0800

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: './',
+  server: {
+    open: true
+  },
+  build: {
+    outDir: 'docs'
+  },
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ZY Player Web',
+        short_name: 'ZY Player'
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    })
+  ]
 })

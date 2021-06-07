94e093ee621da5a43275e61913a437ffed628b07
hunlongyu <hunlongyu@gmail.com>
2021-06-07 10:51:21 +0800

    找到合适的 ts 播放器

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,22 @@
       <el-button @click="settingBtnEvent" icon="el-icon-setting" circle></el-button>
     </el-header>
     <el-main>
-      <el-row class="main" type="flex">
+      <el-row class="input-box" type="flex" v-show="!show.player">
         <el-col :lg="10" :md="12" :sm="16" :xs="24">
-          <el-input placeholder="请输入内容" v-model="uri">
+          <el-input placeholder="请输入内容" v-model.trim="uri" @keydown.enter="enterEvent">
             <template #append>
-              <el-button icon="el-icon-video-play"></el-button>
+              <el-button @click="enterEvent" icon="el-icon-video-play"></el-button>
             </template>
           </el-input>
+        </el-col>
+      </el-row>
+      <el-row class="player-box" type="flex" v-show="show.player">
+        <el-col :lg="10" :md="12" :sm="16" :xs="24" class="box">
+          <Player playsinline controls autoplay ref="player" @vPlaybackReady="onPlaybackReady">
+            <Video>
+              <source data-src="https://media.vimejs.com/720p.mp4" type="video/mp4" />
+            </Video>
+          </Player>
         </el-col>
       </el-row>
     </el-main>
@@ -20,26 +29,56 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, onMounted, reactive, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { Player, Video } from '@vime/vue-next'
+import '@vime/core/themes/default.css'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    Player,
+    Video
+  },
   setup () {
     const { t, locale } = useI18n()
-    const uri = ref('')
+    const uri = ref('') // 用户输入的播放链接
+    const show = reactive({
+      history: false,
+      star: false,
+      setting: false,
+      player: true
+    })
 
+    const player = ref<HTMLVmPlayerElement | null>(null)
+    const src = 'https://zk2.cdt-md.com/2020/12/03/TDJL3BvExyg0muZr/playlist.m3u8'
+    
+    // 历史记录按钮点击事件
     function historyBtnEvent () {}
+    // 收藏记录按钮点击事件
     function starBtnEvent () {}
+    // 设置按钮点击事件
     function settingBtnEvent () {}
+    // 播放事件
+    function enterEvent () {}
+
+    function onPlaybackReady () {
+      console.log('onPlaybackReady')
+    }
+
+    onMounted(() => {})
 
     return {
       t,
       locale,
       uri,
+      player,
+      show,
       historyBtnEvent,
       starBtnEvent,
-      settingBtnEvent
+      settingBtnEvent,
+      enterEvent,
+      onPlaybackReady
     }
   }
 })
